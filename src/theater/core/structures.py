b8f88b002f0c0b10122138ebf77a7e33d3a31322
doremyr3d <francesco@calcagnini.it>
2019-09-30 15:25:51 +0200

    Core struct module

---
+++ src/theater/core/structures.py
@@ -0,0 +1,16 @@
+import collections
+
+
+class DictionaryView(collections.Mapping):
+
+    def __init__(self, data):
+        self.__data = data
+
+    def __getitem__(self, key):
+        return self.__data[key]
+
+    def __len__(self):
+        return len(self.__data)
+
+    def __iter__(self):
+        return iter(self.__data)

580f031a80358cb9b32b6cf78192d7b57defd511
doremyr3d <francesco@calcagnini.it>
2019-10-08 13:36:18 +0200

    (#1) Fix pipeline

--- src/theater/core/errors.py
+++ src/theater/core/errors.py
@@ -3,14 +3,14 @@ import attr
 
 @attr.s(auto_exc=True)
 class IllegalValueException(Exception):
-    msg = attr.ib()
+    msg = attr.ib(default="")
 
 
 @attr.s(auto_exc=True)
 class IllegalActionException(Exception):
-    msg = attr.ib()
+    msg = attr.ib(default="")
 
 
 @attr.s(auto_exc=True)
 class ScoreEnd(Exception):
-    msg = attr.ib()
+    msg = attr.ib(default="End of execution")
